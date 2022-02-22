b86689be9c6d0fb7d4d09f4f3977f7f2a27319c5
beagle <chldusdn20@gmail.com>
2022-02-22 13:40:04 +0900

    test: change code for test in game manager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -17,65 +17,83 @@ namespace Manager
         [SerializeField] private Camera mainCamera;
 
         private CameraFollow cameraFollow;
-        
+
+        private List<Enemy> enemyList;
+        private List<Hero> heroList;
+
         private void Start()
         {
             cameraFollow = mainCamera.GetComponent<CameraFollow>();
 
+            enemyList = new List<Enemy>();
+            heroList = new List<Hero>();
+
             var map = Instantiate(GameAssets.i.pfMap1, Vector3.zero, Quaternion.identity);
             PathfindingManager.Instance.Init();
             
-            var mainHero = Hero.Create(new Vector3(0, 19, 0), GameAssets.i.pfBachi, HeroControlType.Joystick, WeaponType.Arrow, true);
-            //Hero.Create(new Vector3(0, 22, 0), GameAssets.i.pfDuju, HeroControlType.Auto, WeaponType.Sword, true);
-            //Hero.Create(new Vector3(0, 16, 0), GameAssets.i.pfPanno, HeroControlType.Auto, WeaponType.Arrow, true);
-            //Hero.Create(new Vector3(-3, 22, 0), GameAssets.i.pfBachi, ControlType.Auto, WeaponType.Spear, true);
-            //Hero.Create(new Vector3(-3, 19, 0), GameAssets.i.pfDuju, HeroControlType.Auto, WeaponType.Spear, true);
-            //Hero.Create(new Vector3(-3, 16, 0), GameAssets.i.pfPanno, HeroControlType.Auto, WeaponType.Sword, true);
+            var mainHero = Hero.Create(new Vector3(0, 19, 0), GameAssets.i.pfBachi, HeroControlType.Joystick, WeaponType.Arrow);
+            //Hero.Create(new Vector3(0, 22, 0), GameAssets.i.pfDuju, HeroControlType.Auto, WeaponType.Sword, false);
+            //Hero.Create(new Vector3(0, 16, 0), GameAssets.i.pfPanno, HeroControlType.Auto, WeaponType.Arrow, false);
+            //Hero.Create(new Vector3(-3, 22, 0), GameAssets.i.pfBachi, ControlType.Auto, WeaponType.Spear, false);
+            //Hero.Create(new Vector3(-3, 19, 0), GameAssets.i.pfDuju, HeroControlType.Auto, WeaponType.Spear, false);
+            //Hero.Create(new Vector3(-3, 16, 0), GameAssets.i.pfPanno, HeroControlType.Auto, WeaponType.Sword, false);
             
+            heroList.Add(mainHero);
             
-            Enemy.Create(new Vector3(6, 19, 0), GameAssets.i.pfEnemy1, WeaponType.Spear, EnemyActionType.Detect, new List<HeroControlType>
-                {
-                    HeroControlType.Joystick,
-                    HeroControlType.Auto
-                }, true);
+            enemyList.Add(Enemy.Create(new Vector3(6, 19, 0), GameAssets.i.pfEnemy1, WeaponType.Spear, EnemyActionType.Attack, new List<HeroControlType>
+            {
+                HeroControlType.Joystick,
+                HeroControlType.Auto
+            }));
             
-            Enemy.Create(new Vector3(6, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Detect, new List<HeroControlType>
-                {
-                    HeroControlType.Joystick,
-                    HeroControlType.Auto
-                }, true); 
-            Enemy.Create(new Vector3(6, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Detect, new List<HeroControlType>
-                {
-                    HeroControlType.Joystick,
-                    HeroControlType.Auto
-                }, true);
-            Enemy.Create(new Vector3(6, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Spear, EnemyActionType.Detect,  new List<HeroControlType>
+            enemyList.Add(Enemy.Create(new Vector3(6, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Attack, new List<HeroControlType>
+            {
+                HeroControlType.Joystick,
+                HeroControlType.Auto
+            }));
+            
+            enemyList.Add(Enemy.Create(new Vector3(6, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Attack, new List<HeroControlType>
+            {
+                HeroControlType.Joystick,
+                HeroControlType.Auto
+            }));
+            
+            enemyList.Add(
+                Enemy.Create(new Vector3(6, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Spear, EnemyActionType.Attack,  new List<HeroControlType>
                 {
                     HeroControlType.Auto,
                     HeroControlType.Joystick
-                }, true);
-            Enemy.Create(new Vector3(6, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Detect,  new List<HeroControlType>
+                })
+            );
+            enemyList.Add(
+                Enemy.Create(new Vector3(6, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Attack,  new List<HeroControlType>
                 {
                     HeroControlType.Auto,
                     HeroControlType.Joystick
-                }, true);
-            Enemy.Create(new Vector3(6, 22, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Detect,  new List<HeroControlType>
+                })
+            );
+            enemyList.Add(
+                Enemy.Create(new Vector3(6, 22, 0), GameAssets.i.pfEnemy1, WeaponType.Arrow, EnemyActionType.Attack,  new List<HeroControlType>
                 {
                     HeroControlType.Auto,
                     HeroControlType.Joystick
-                }, true);
-            Enemy.Create(new Vector3(9, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Detect,  new List<HeroControlType>
+                })
+            );
+            enemyList.Add(
+                Enemy.Create(new Vector3(9, 16, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Attack,  new List<HeroControlType>
                 {
                     HeroControlType.Auto,
                     HeroControlType.Joystick
-                }, true);
-            Enemy.Create(new Vector3(9, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Detect,  new List<HeroControlType>
+                })
+            );
+            enemyList.Add(
+                Enemy.Create(new Vector3(9, 13, 0), GameAssets.i.pfEnemy1, WeaponType.Sword, EnemyActionType.Attack,  new List<HeroControlType>
                 {
                     HeroControlType.Auto,
                     HeroControlType.Joystick
-                }, true);
-            
-            
+                })  
+            );
+
             cameraFollow.Init(mainHero.gameObject.transform);
         }
 
@@ -84,6 +102,17 @@ namespace Manager
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 UnityEditor.EditorApplication.isPlaying = false;
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                foreach (var enemy in enemyList)
+                {
+                    enemy.Init();
+                }
+                foreach (var hero in heroList)
+                {
+                    hero.Init();
+                }
             }
         }
     }
